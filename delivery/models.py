3b7aee58fc174f03879a6c268fc36308b0290b60
Arian Hedayati Far <arian.hedayatifar@gmail.com>
2021-03-14 14:09:00 +0330

    added Package model

--- delivery/models.py
+++ delivery/models.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
-
+from django.core.validators import RegexValidator
 
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("created at"))
@@ -46,3 +46,71 @@ class Courier(User):
 
 class Collection(User):
     pass
+
+class Package(SluggedModel):
+    sender = models.ForeignKey(
+        "Collection",
+        on_delete=models.SET_NULL,
+        verbose_name=_("sender"),
+        help_text=_("sender"),
+        db_index=True,
+    )
+    courier = models.ForeignKey(
+        "Courier",
+        on_delete=models.SET_NULL,
+        verbose_name=_("sender"),
+        help_text=_("sender"),
+        db_index=True,
+    )
+    phone_regex = RegexValidator(
+        regex=r"^09\d{9}$",
+        message=_("phone number must be entered in the format: '09---------'."),
+    )
+    sender_phone_number = models.CharField(
+        max_length=16,
+        validators=[phone_regex],
+        verbose_name=_("sender phone number"),
+    )
+    sender_name = models.CharField(
+        max_length=255,
+        verbose_name=_("sender name")
+    )
+    receiver_phone_number = models.CharField(
+        max_length=16,
+        validators=[phone_regex],
+        verbose_name=_("receiver phone number"),
+    )
+    receiver_name = models.CharField(
+        max_length=255,
+        verbose_name=_("receiver name")
+    )
+    origin_long = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        blank=False,
+        verbose_name=_("origin longtitude")
+    )
+    origin_lat = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        blank=False,
+        verbose_name=_("origin latitude")
+    )
+    origin_address = models.TextField(
+        verbose_name=_("origin address"),
+    )
+    destination_long = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        blank=False,
+        verbose_name=_("destination longtitude")
+    )
+    destination_lat = models.DecimalField(
+        max_digits=9,
+        decimal_places=6,
+        blank=False,
+        verbose_name=_("destination latitude")
+    )
+    destination_address = models.TextField(
+        verbose_name=_("address"),
+    )
