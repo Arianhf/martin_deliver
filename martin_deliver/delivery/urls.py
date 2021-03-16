5811ba08de7ea549ee57802bee2cd6eec29bda92
Arian Hedayati Far <arian.hedayatifar@gmail.com>
2021-03-16 19:32:32 +0330

    added url for package create and collection list

--- martin_deliver/delivery/urls.py
+++ martin_deliver/delivery/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('courier/', views.CourierCreate.as_view()),
     path('courier/list/', views.CourierList.as_view()),
     path('collection/', views.CollectionCreate.as_view()),
+    path('collection/list/', views.CollectionList.as_view()),
+    path('package/', views.PackageCreate.as_view()),
+
 
 #    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
 ]
