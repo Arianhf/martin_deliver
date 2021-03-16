1bdcc05dc7829d077fdc2991e9eb95442d47cf81
Arian Hedayati Far <arian.hedayatifar@gmail.com>
2021-03-16 21:04:14 +0330

    added cancel package url

--- martin_deliver/delivery/urls.py
+++ martin_deliver/delivery/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     path('courier/list/', views.CourierList.as_view()),
     path('collection/', views.CollectionCreate.as_view()),
     path('collection/list/', views.CollectionList.as_view()),
+    path('package/<slug:slug>/cancel/', views.PackageCancel.as_view()), 
     path('package/', views.PackageCreate.as_view()),
+    
 
 
 #    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
