322a6985f8f017e964ae7714fbb5395bbe955305
Arian Hedayati Far <arian.hedayatifar@gmail.com>
2021-03-16 19:34:46 +0330

    added package create and collection list views

--- martin_deliver/delivery/views.py
+++ martin_deliver/delivery/views.py
@@ -1,12 +1,20 @@
+from .models import Courier, Collection, Package
+from .serializers import (
+    CourierSignupSerializer,
+    CourierSerializer,
+    CollectionSerializer,
+    CollectionSignupSerializer,
+    PackageCreateSerializer,
+    CustomTokenObtainPairSerializer
+)
+from rest_framework import status, permissions, mixins, generics
+from .permissions import IsCollection
 
-from .models import Courier
-from .serializers import CourierSignupSerializer, CourierSerializer, CollectionSerializer, CollectionSignupSerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework import mixins
-from rest_framework import generics
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class EmailTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
 
 class CourierCreate(mixins.CreateModelMixin, generics.GenericAPIView):
     queryset = Courier.objects.all()
@@ -15,6 +23,7 @@ class CourierCreate(mixins.CreateModelMixin, generics.GenericAPIView):
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
 
+
 class CourierList(mixins.ListModelMixin, generics.GenericAPIView):
     queryset = Courier.objects.all()
     serializer_class = CourierSerializer
@@ -22,9 +31,29 @@ class CourierList(mixins.ListModelMixin, generics.GenericAPIView):
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
 
+
 class CollectionCreate(mixins.CreateModelMixin, generics.GenericAPIView):
     queryset = Courier.objects.all()
     serializer_class = CollectionSignupSerializer
 
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
+
+class CollectionList(mixins.ListModelMixin, generics.GenericAPIView):
+    queryset = Collection.objects.all()
+    serializer_class = CollectionSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+class PackageCreate(mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Package.objects.all()
+    serializer_class = PackageCreateSerializer
+    permission_classes = [IsCollection]
+
+    def post(self, request, *args, **kwargs):
+        self.serializer = self.get_serializer(data=request.data)
+        self.serializer.user = request.user
+        return self.create(request, *args, **kwargs)
+
+    
